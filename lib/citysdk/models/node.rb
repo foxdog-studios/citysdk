70f4c0d3725fcd8301c28e130bf03e531220b899
Lloyd Henning <lloyd.w.henning@gmail.com>
2014-04-04 15:45:43 +0100

    Moved serialization out of node

--- lib/citysdk/models/node.rb
+++ lib/citysdk/models/node.rb
@@ -4,26 +4,6 @@ module CitySDK
   class Node < Sequel::Model
     one_to_many :node_data
 
-    def self.processPredicate(n,params)
-      p = params[:p]
-      layer,field = p.split('/')
-      if 0 == Layer.where(:name=>layer).count
-        CitySDKAPI.do_abort(422,"Layer not found: 'layer'")
-      end
-      layer_id = Layer.idFromText(layer)
-      nd = NodeDatum.where({:node_id => n[:id], :layer_id => layer_id}).first
-      if nd
-        # puts JSON.pretty_generate(nd[:data])
-        case params[:request_format]
-        when'application/json'
-          @@noderesults << {field => nd[:data][field.to_sym]}
-        when'text/turtle'
-          @@noderesults = NodeDatum.turtelizeOneField(n[:cdk_id],nd,field,params)
-        end
-      end
-    end
-
-
     def getLayer(n)
       if n.is_a?(String)
         self.node_data.each do |nd|
@@ -38,109 +18,6 @@ module CitySDK
     end
 
 
-
-
-
-
-
-
-
-    def self.serialize(h,params)
-      case params[:request_format]
-      when 'application/json'
-        Node.make_hash(h,params)
-      when 'text/turtle'
-        Node.turtelize(h,params)
-      end
-    end
-
-
-    def self.make_hash(h,params)
-      h[:layers] = NodeDatum.serialize(h[:cdk_id], h[:node_data], params) if h[:node_data]
-      # members not directly exposed,
-      # call ../ptstops form members of route, f.i.
-      h.delete(:members)
-
-      h[:layer] = Layer.nameFromId(h[:layer_id])
-      h[:name] = '' if h[:name].nil?
-      if params.has_key? "geom"
-        if h[:member_geometries] and h[:node_type] != 3
-          h[:geom] = RGeo::GeoJSON.encode(CitySDKAPI.rgeo_factory.parse_wkb(h[:member_geometries])) if h[:member_geometries]
-        elsif h[:geom]
-          h[:geom] = RGeo::GeoJSON.encode(CitySDKAPI.rgeo_factory.parse_wkb(h[:geom])) if h[:geom]
-        end
-      else
-        h.delete(:geom)
-      end
-
-      if h[:modalities]
-        h[:modalities] = h[:modalities].map { |m| Modality.name_for_id(m) }
-      else
-        h.delete(:modalities)
-      end
-
-      h.delete(:related) if h[:related].nil?
-      h.delete(:member_geometries)
-      #h.delete(:modalities) if (h[:modalities] == [] or h[:modalities].nil?)
-      h[:node_type] = @@node_types[h[:node_type]]
-      h.delete(:layer_id)
-      h.delete(:id)
-      h.delete(:node_data)
-      h.delete(:created_at)
-      h.delete(:updated_at)
-
-      if h.has_key? :collect_member_geometries
-        h.delete(:collect_member_geometries)
-      end
-      @@noderesults << h
-      h
-    end
-
-
-    def self.turtelize(h, params)
-      @@prefixes << 'rdfs:'
-      @@prefixes << 'rdf:'
-      @@prefixes << 'geos:'
-      @@prefixes << 'dc:'
-      @@prefixes << 'owl:'
-      @@prefixes << 'lgdo:' if h[:layer_id] == 0
-      triples = []
-
-      if not @@layers.include?(h[:layer_id])
-        @@layers << h[:layer_id]
-        triples << "<layer/#{Layer.nameFromId(h[:layer_id])}> a :Layer ."
-        triples << ""
-      end
-
-      triples << "<#{h[:cdk_id]}>"
-      triples << "\t a :#{@@node_types[h[:node_type]].capitalize} ;"
-      triples << "\t dc:title \"#{h[:name].gsub('"','\"')}\" ;" if h[:name] and h[:name] != ''
-      triples << "\t :createdOnLayer <layer/#{Layer.nameFromId(h[:layer_id])}> ;"
-
-      if h[:modalities]
-        h[:modalities].each { |m|
-          triples << "\t :hasTransportmodality :transportModality_#{Modality.name_for_id(m)} ;"
-        }
-      end
-
-      if params.has_key? "geom"
-        if h[:member_geometries] and h[:node_type] != 3
-          triples << "\t geos:hasGeometry \"" +  RGeo::WKRep::WKTGenerator.new.generate( CitySDKAPI.rgeo_factory.parse_wkb(h[:member_geometries]) )  + "\" ;"
-        elsif h[:geom]
-          triples << "\t geos:hasGeometry \"" +  RGeo::WKRep::WKTGenerator.new.generate( CitySDKAPI.rgeo_factory.parse_wkb(h[:geom]) )  + "\" ;"
-        end
-      end
-
-      if h[:node_data]
-        t,d =  NodeDatum.turtelize(h[:cdk_id], h[:node_data], params)
-        triples += t if t
-        triples += d if d
-      end
-
-      @@noderesults += triples
-      @@noderesults[-1][-1]='.' if @@noderesults[-1] and @@noderesults[-1][-1] == ';'
-      triples
-    end # def
   end # class
 end # module
 
