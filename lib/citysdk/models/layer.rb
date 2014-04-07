b0b5f3c957e0787532aa1d63d4c6d05160934053
Peter Sutton <foxxy@foxdogstudios.com>
2014-04-07 14:32:23 +0100

    Added ORM link between layers and layer properties

--- lib/citysdk/models/layer.rb
+++ lib/citysdk/models/layer.rb
@@ -6,8 +6,8 @@ module CitySDK
     # and cannot be deleted.
     UNDELETABLE_IDS = Set.new([0, 1, 2])
 
-
     many_to_one :owner, class: :SequelUser
+    one_to_many :layer_properties
     one_to_many :node_data
 
     def deletable?
