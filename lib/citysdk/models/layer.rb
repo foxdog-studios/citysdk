1bffb9064ed15a8c1064be6aab117a6c6ceb61a2
Peter Sutton <foxxy@foxdogstudios.com>
2014-04-15 10:02:47 +0100

    Organized the Layer class

--- lib/citysdk/models/layer.rb
+++ lib/citysdk/models/layer.rb
@@ -2,15 +2,13 @@
 
 module CitySDK
   class Layer < Sequel::Model
-    # As of 8th Feb 2014, the layers with IDs 0, 1, and 2 are required
-    # and cannot be deleted.
-    UNDELETABLE_IDS = Set.new([0, 1, 2])
+    plugin :validation_helpers
 
     many_to_one :owner, class: :SequelUser
     one_to_many :layer_properties
     one_to_many :node_data
 
-    def deletable?
+    def deletable?()
       !UNDELETABLE_IDS.include?(id)
     end # def
 
@@ -19,53 +17,46 @@ module CitySDK
       return parts.first() if parts.length > 1
     end # def
 
+    def validate()
+      super()
+      validate_domain()
+      validate_name()
+      validate_presence()
+    end # def
+
     def self.for_name(name)
-      where(name: name).first
+      where(name: name).first()
     end # def
 
-    def self.get_layers_in_category(category)
-      where(Sequel.like(:category, "#{ category }%"))
+    private
+
+    # As of 8th Feb 2014, the layers with IDs 0, 1, and 2 are required
+    # and cannot be deleted.
+    UNDELETABLE_IDS = Set.new([0, 1, 2]).freeze()
+
+    def validate_domain()
+      return if owner.site_admin?
+      domain = root_domain()
+      if domain.nil?
+        errors.add(:domain, 'cannot be empty')
+      elsif !owner.domains.include?(domain)
+        errors.add(:domain, 'user must be a memeber of the domain')
+      end # if
     end # def
-  end # class
 
+    def validate_name()
+      validates_format(/^\w+(\.\w+)*$/, :name)
+      validates_unique(:name)
+    end # def
 
-  # Stuff Peter has not looked at.
-  class Layer < Sequel::Model
-    plugin :validation_helpers
-    plugin :json_serializer
-
-    KEY_LAYER_NAMES = 'layer_names'
-    KEY_LAYERS_AVAILABLE = 'layers_available'
-
-    def validate
-      super
-      validates_presence [
+    def validate_presence()
+      validates_presence([
         :category,
         :description,
         :name,
         :organization
-      ]
-      validates_unique :name
-      validates_format /^\w+(\.\w+)*$/, :name
-
-      domain = root_domain()
-
-      if !owner.site_admin?
-        if domain.nil?
-          errors.add(:domain, 'cannot be empty')
-        elsif !owner.domains.include?(domain)
-          errors.add(:domain, 'user must be a memeber of the domain')
-        end # if
-      end # if
-    end
-
-    def self.memcache_key(id)
-      "layer!!#{ id }"
-    end
-
-    def self.get_layer(id)
-      self[id]
-    end
+      ])
+    end # def
   end # class
 end # module
 
